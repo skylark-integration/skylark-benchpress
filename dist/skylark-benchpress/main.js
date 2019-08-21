b9c6157f38bb5f817afb6f78a63824e85ff3ceb0
hudaokeji <github@hudaokeji.com>
2019-08-22 01:39:51 +0900

    update

--- dist/skylark-benchpress/main.js
+++ dist/skylark-benchpress/main.js
@@ -1,5 +1,5 @@
 /**
- * skylark-jade - A version of benchpress.js that ported to running on skylarkjs
+ * skylark-benchpress - A version of benchpress.js that ported to running on skylarkjs
  * @author Hudaokeji, Inc.
  * @version v0.9.0
  * @link https://github.com/skylark-integration/skylark-benchpress/
