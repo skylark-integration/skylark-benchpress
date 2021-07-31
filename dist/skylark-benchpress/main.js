10e75bb5df22a04b83199e982e0808e29b6ca041
hudaokeji <github@hudaokeji.com>
2021-07-31 16:15:05 +0900

    update

--- dist/skylark-benchpress/main.js
+++
@@ -1,9 +0,0 @@
-/**
- * skylark-benchpress - A version of benchpress.js that ported to running on skylarkjs
- * @author Hudaokeji, Inc.
- * @version v0.9.0
- * @link https://github.com/skylark-integration/skylark-benchpress/
- * @license MIT
- */
-define(["./benchpress"],function(e){return e});
-//# sourceMappingURL=sourcemaps/main.js.map
--- dist/uncompressed/skylark-benchpress/benchpress.js
+++ dist/uncompressed/skylark-benchpress/benchpress.js
@@ -1,5 +1,5 @@
 define([
-  "skylark-langx/skylark"
+  "skylark-langx-ns"
 ],function (skylark) {
   var runtime = function () {
     'use strict';
